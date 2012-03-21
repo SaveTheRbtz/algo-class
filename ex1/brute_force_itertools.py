1e0d6e2f066b080186ca296957fab83b8ca9fd1d
Alexey Ivanov <SaveTheRbtz@GMail.com>
2012-03-21 04:45:30 +0400

    [ex1] Itertools version of brute_force
    It 2.5 times faster that plain Python version.
    It also proves that not only algorithm that matters.

---
+++ ex1/brute_force_itertools.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from common import parse_file
+import itertools
+
+def brute_force(data):
+    """
+    This function should return number of inversions of input stream
+
+    >>> brute_force([1,2,3,4,5,6])
+    0
+    >>> brute_force([1,3,5,2,4,6])
+    3
+    >>> brute_force([6,5,4,3,2,1])
+    15
+    """
+    return sum(itertools.ifilter(None, (a>b for a,b in itertools.combinations(data, 2))))
+
+if __name__ == '__main__':
+    from optparse import OptionParser
+    parser = OptionParser()
+    parser.add_option("-f", type="string", dest="file", default="IntegerArray.txt")
+    (options, args) = parser.parse_args()
+
+    print brute_force(parse_file(options.file))
